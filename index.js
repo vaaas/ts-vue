4a981d9ea98412b3840903607efc9a5ce9b3f3c7
Vasileios Pasialiokis <whiterocket@outlook.com>
2022-07-27 11:12:14 +0300

    improvements

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 import { h as vh } from 'vue';
+/**
+ * Turn a string or a Vue component into a virtual DOM node
+ * for rendering in Vue.
+ */
 export function h(elem, props, slots) {
     return vh(
     // @ts-ignore
     elem, props, slots);
 }
-h(component, {
-    onValue: (x) => console.log(x),
-});
+/**
+ * Converts a Vue component with enhanced type checking
+ * to a regular Vue component, while maintining the types.
+ */
+export function defineComponent(x) {
+    return {
+        render: x.render,
+        setup: x.setup,
+        beforeCreate: x.beforeCreate,
+        created: x.created,
+        beforeMount: x.beforeMount,
+        mounted: x.mounted,
+        beforeUnmount: x.beforeUnmount,
+        unmounted: x.unmounted,
+        beforeUpdate: x.beforeUpdate,
+        updated: x.updated,
+        activated: x.activated,
+        deactivated: x.deactivated,
+        data: x.data,
+        computed: x.computed,
+        watch: x.watch,
+        props: Object.keys(x)
+    };
+}
--- index.ts
+++ index.ts
@@ -2,7 +2,33 @@ import { h as vh, VNode } from 'vue'
 
 type HTMLElements = 'div' | 'span' | 'p' | 'em' | 'strong' | 'input'
 
-type Component = { render(): VNode|string }
+type Component = {
+    render(): VNode|string;
+
+    setup?: () => void;
+    beforeCreate?: () => void;
+    created?: () => void;
+    beforeMount?: () => void;
+    mounted?: () => void;
+    beforeUnmount?: () => void;
+    unmounted?: () => void;
+    beforeUpdate?: () => void;
+    updated?: () => void;
+    activated?: () => void;
+    deactivated?: () => void;
+
+    data?: () => Record<string, any>;
+
+    computed?: Record<string, () => any>;
+
+    watch?: Record<string, (ov: any, nv: any) => void>;
+
+    emits?: Record<string, any>;
+
+    slots?: Record<string, Array<any>>;
+
+    props?: Record<string, any>;
+}
 
 type DefaultProps<T extends HTMLElements> = {
     class?: string,
@@ -16,23 +42,53 @@ type On<T extends Record<string, any>> = {
 type Props<T extends object> =
     DefaultProps<HTMLElements>
     &
-    (T extends { props: infer U } ? U : {})
+    (T extends { props: infer U extends Record<string, any> } ? Partial<U> : {})
     &
-    (T extends { emits: infer U extends Record<string, any> } ? On<U> : {})
+    (T extends { emits: infer U extends Record<string, any> } ? Partial<On<U>> : {})
 
-type Slots<T extends object> = T extends { slots: infer U extends Array<string> }
-    ? {[k in U[number]]: () => VNode|string }:
-    null
+type Slots<T extends object> =
+    T extends { slots: infer U extends Record<string, Array<any>> }
+    ? {[k in string & keyof U]: (...xs: U[k]) => VNode|string }
+    : null
 
+/**
+ * Helper type to calculate the `this` interface of a Vue component.
+ */
 export type This<T extends object> =
-    (T extends { data: infer U extends object } ? U : {})
+    (
+        T extends { data: infer U extends object }
+        ? U
+        : {}
+    )
     &
-    (T extends { computed: infer U extends Record<string, () => any>} ? {[K in string & keyof U]: ReturnType<U[K]> } : {})
+    (
+        T extends { slots: infer U extends Record<string, any> }
+        ? {[K in string & keyof U]: U[K]}
+        : {}
+    )
     &
-    (T extends { methods: infer U extends Record<string, Function> } ? U : {})
+    (
+        T extends { computed: infer U extends Record<string, () => any>}
+        ? {[K in string & keyof U]: ReturnType<U[K]> }
+        : {}
+    )
     &
-    (T extends { emits: infer U extends object } ? {$emit: <K extends keyof U>(event: K, value: U[K]) => void } : {})
+    (
+        T extends { methods: infer U extends Record<string, Function> }
+        ? U
+        : {}
+    )
+    &
+    (
+        T extends { emits: infer U extends object }
+        ? {$emit: <K extends keyof U>(event: K, value: U[K]) => void }
+        : {}
+    )
 
+/**
+ * Turn a string or a Vue component into a virtual DOM node
+ * for rendering in Vue.
+ */
 export function h<T extends HTMLElements | Component>(
     elem: T,
     props?: T extends HTMLElements
@@ -54,16 +110,27 @@ export function h<T extends HTMLElements | Component>(
     )
 }
 
-declare const component: {
-    render: () => 'yo',
-    emits: {
-        'value': number
-    }
+/**
+ * Converts a Vue component with enhanced type checking
+ * to a regular Vue component, while maintining the types.
+ */
+export function defineComponent<T extends Component>(x: T) {
+    return {
+        render: x.render,
+        setup: x.setup,
+        beforeCreate: x.beforeCreate,
+        created: x.created,
+        beforeMount: x.beforeMount,
+        mounted: x.mounted,
+        beforeUnmount: x.beforeUnmount,
+        unmounted: x.unmounted,
+        beforeUpdate: x.beforeUpdate,
+        updated: x.updated,
+        activated: x.activated,
+        deactivated: x.deactivated,
+        data: x.data,
+        computed: x.computed,
+        watch: x.watch,
+        props: Object.keys(x)
+    } as any as T
 }
-
-h(
-    component,
-    {
-        onValue: (x) => console.log(x),
-    },
-)
